6416bd69c1546210f2ed3caba6968986a5940a3f
terehova <aterehova@awg.ru>
2025-12-12 12:27:34 +0300

    5 задач из 1-го урока

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowerStr = str.toLowerCase();
+  return lowerStr.includes('1xbet') || lowerStr.includes('xxx');
 }
+
+console.log(checkSpam('1XbeT now'));
+console.log(checkSpam('free XXXX'));
+console.log(checkSpam('innocent rabbit'));
